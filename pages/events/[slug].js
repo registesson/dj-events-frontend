5e5211fd009d5d2e9b9bf48fe76b55df5fd3a38a
Regis Tesson <registesson@gmail.com>
2021-09-30 13:57:05 +0200

    Switch to getServerSideProps in slug page

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -47,6 +47,7 @@ export default function EventPage({ evt }) {
   )
 }
 
+/**
 export async function getStaticPaths() {
   const res = await fetch(`${API_URL}/events`)
   const events = await res.json()
@@ -64,6 +65,18 @@ export async function getStaticPaths() {
 export async function getStaticProps({ params: { slug } }) {
   const res = await fetch(`${API_URL}/events?slug=${slug}`)
   const events = await res.json()
+  return {
+    props: {
+      evt: events[0],
+    },
+  }
+}
+
+*/
+export async function getServerSideProps({ query: { slug } }) {
+  const res = await fetch(`${API_URL}/events?slug=${slug}`)
+  const events = await res.json()
+
   return {
     props: {
       evt: events[0],
